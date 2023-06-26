e8364b029942a55661a4264fd38074f4123f9e4b
Alivapupul <115943981+Alivapupul@users.noreply.github.com>
2023-06-26 23:11:49 +0530

    MySql db bug fix

--- main.py
+++ main.py
@@ -9,67 +9,62 @@ app.secret_key = 'alivaKey'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = 'Aliva@2001'
 app.config['MYSQL_DB'] = 'pythonlogin'
 
 mysql = MySQL(app)
 
-#login
+#loginCheck
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
     msg = ''
+    # Check if form already submitted
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-       
         username = request.form['username']
         password = request.form['password']
 
-        hash = password + app.secret_key
-        hash = hashlib.sha1(hash.encode())
-        password = hash.hexdigest()
-        
-        # Using MySQL check account exists or not
+        # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch the record and return the result
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
         account = cursor.fetchone()
-        
-        # If account exists in table show in database
-        if account:
-            # Create session data, we can access this data in other routes
+
+        # If account exists 
+        if account and hashlib.sha1(password.encode() + app.secret_key.encode()).hexdigest() == account['password']:
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
+            return redirect(url_for('form'))
         else:
-            # Account doesn't exist 
             msg = 'Incorrect username/password!'
+    return render_template('index.html', msg=msg)
 
-#logout
+#logoutCheck
 @app.route('/pythonlogin/logout')
 def logout():
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
-   return redirect(url_for('login'))
+
+   #go back to login page
+   return redirect(url_for('login')) 
 
 #register
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+
+    # Check if form already submitted
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # For easy access create variables
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
         
-        # Check if account exists using MySQL
+        # Check if account exists
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
         account = cursor.fetchone()
-        # If account exists show error and validation checks
+
+        # If account exists show error
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -79,65 +74,52 @@ def register():
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            # Hash the password
             hash = password + app.secret_key
             hash = hashlib.sha1(hash.encode())
             password = hash.hexdigest()
-            # Account doesn't exist, and the form data is valid, insert the new account into the table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            
+            # Add to database
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
+        
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
 
-# Grocery Store page
-@app.route('/pythonlogin/home')
-def home():
-    # Check if the user is logged in
+@app.route('/pythonlogin/form', methods=['GET', 'POST'])
+def form():
     if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
-    # User is not loggedin redirect to login page
+        return render_template('form.html', username=session['username'])
+    
     return redirect(url_for('login'))
 
-# Connect to the MySQL database
-db = mysql.connector.connect(
-    host='local_host',
-    user='root',
-    password='password',
-    database='pythonlogin'
-)
-
-# Define a route for the form page
-@app.route('/')
-def form():
-    return render_template('home.html')
-# Define a route to handle form submission
-@app.route('/add_item', methods=['POST'])
+@app.route('/pythonlogin/add_item', methods=['GET', 'POST'])
 def add_item():
-    # Retrieve the form data
-    item_name = request.form['item_name']
-    item_type = request.form['item_type']
-    quantity = int(request.form['quantity'])
-    price = float(request.form['price'])
 
-    # Insert the item into the database
-    cursor = db.cursor()
-    query = "INSERT INTO items (name, type, quantity, price) VALUES (%s, %s, %s, %s)"
-    values = (item_name, item_type, quantity, price)
-    cursor.execute(query, values)
-    db.commit()
-    cursor.close()
+    if 'loggedin' in session:
 
-    return "Item added successfully!"
-# Define a route to handle table display
-@app.route('/table')
+        # Get the form data
+        item_name = request.form['item_name']
+        item_type = request.form['item_type']
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+
+        # Add to database
+        cursor = mysql.connection.cursor()
+        query = "INSERT INTO items (item_name, item_type, quantity, price) VALUES (%s, %s, %s, %s)"
+        values = (item_name, item_type, quantity, price)
+        cursor.execute(query, values)
+        mysql.connection.commit()
+        cursor.close()
+        msg = 'Item added successfully!'
+        
+        return render_template('form.html', msg=msg)
+    
+@app.route('/pythonlogin/table', methods=['GET', 'POST'])
 def display_table():
-    cursor = db.cursor()
-    query = "SELECT * FROM items"
+    cursor = mysql.connection.cursor()
+    query = "SELECT item_name, item_type, quantity, price FROM items"
     cursor.execute(query)
     rows = cursor.fetchall()
     cursor.close()
